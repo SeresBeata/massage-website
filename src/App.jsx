c76535f5ac5e6984d9bd9a65b8670954ba8a65b9
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-05-20 23:35:34 +0200

    Update App.jsx file
    Update  src of img at App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import './App.css'
 const App = () => {
   return (
     <>
-      <h1>Hello World!</h1>
-      <img style={{ width: '200px' }} src="/img/pexels-olly-3757952.jpg" />
+      <h1>Hello Worlcnfvf!</h1>
+      <img
+        style={{ width: '200px' }}
+        src="https://github.com/SeresBeata/massage-website/blob/main/public/img/pexels-olly-3757952.jpg?raw=true"
+      />
     </>
   )
 }
