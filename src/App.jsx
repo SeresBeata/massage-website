45c02a30f88362bcd905323871eafcff8e08c371
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-09-23 17:40:40 +0200

    Update useEffect() for images and videos
    Comment out previous useEffect() and create new one for getting images and videos at the same time.

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,63 @@ const App = () => {
 
   const [videos, setVideos] = useState([])
 
+  useEffect(() => {
+    if (images.length === 0) {
+      getImages()
+    }
+    getVideos()
+  }, [nextPage, perPage])
+
+  const getImages = async () => {
+    setLoading(true)
+    await fetch
+      .get(`https://api.pexels.com/v1/search?query=$massage&per_page=20`)
+      .then((res) => {
+        setImages([...images, ...res.data.photos])
+        setLoading(false)
+        console.log(res)
+      })
+      .catch((er) => {
+        if (er.response) {
+          const error =
+            er.response.status === 404
+              ? 'Page not found'
+              : 'Something wrong has happened'
+          setError(error)
+          setLoading(false)
+          console.log(error)
+        }
+      })
+  }
+
+  const getVideos = async () => {
+    setLoading(true)
+    await fetch
+      .get(
+        `https://api.pexels.com/videos/search?query=massage&page=${nextPage}&per_page=${perPage}`
+      )
+      .then((res) => {
+        return res
+      })
+      .then((data) => {
+        console.log(data)
+        setVideos(data.data.videos)
+        setLoading(false)
+      })
+      .catch((er) => {
+        console.log(er)
+        if (er.response) {
+          const error =
+            er.response.status === 404
+              ? 'Page not found'
+              : 'Something wrong has happened'
+          setError(error)
+          setLoading(false)
+          console.log(error)
+        }
+      })
+  }
+
   // useEffect(() => {
   //   const getImages = async () => {
   //     setLoading(true)
@@ -37,36 +94,36 @@ const App = () => {
   //   getImages()
   // }, [nextPage, perPage])
 
-  useEffect(() => {
-    const getVideos = async () => {
-      setLoading(true)
-      await fetch
-        .get(
-          `https://api.pexels.com/videos/search?query=massage&page=${nextPage}&per_page=${perPage}`
-        )
-        .then((res) => {
-          return res
-        })
-        .then((data) => {
-          console.log(data)
-          setVideos(data.data.videos)
-          setLoading(false)
-        })
-        .catch((er) => {
-          console.log(er)
-          if (er.response) {
-            const error =
-              er.response.status === 404
-                ? 'Page not found'
-                : 'Something wrong has happened'
-            setError(error)
-            setLoading(false)
-            console.log(error)
-          }
-        })
-    }
-    getVideos()
-  }, [nextPage, perPage])
+  // useEffect(() => {
+  //   const getVideos = async () => {
+  //     setLoading(true)
+  //     await fetch
+  //       .get(
+  //         `https://api.pexels.com/videos/search?query=massage&page=${nextPage}&per_page=${perPage}`
+  //       )
+  //       .then((res) => {
+  //         return res
+  //       })
+  //       .then((data) => {
+  //         console.log(data)
+  //         setVideos(data.data.videos)
+  //         setLoading(false)
+  //       })
+  //       .catch((er) => {
+  //         console.log(er)
+  //         if (er.response) {
+  //           const error =
+  //             er.response.status === 404
+  //               ? 'Page not found'
+  //               : 'Something wrong has happened'
+  //           setError(error)
+  //           setLoading(false)
+  //           console.log(error)
+  //         }
+  //       })
+  //   }
+  //   getVideos()
+  // }, [nextPage, perPage])
 
   const handleLoadMoreClick = () => setNextPage(nextPage + 1)
 
