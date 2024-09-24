a4e4d0365c5b0f2acf130cc6bcb95fdd2c2961ec
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-09-24 17:29:13 +0200

    Create Symbol.jsx
    Create and export Symbol component with div elements.

---
+++ src/components/Symbol/Symbol.jsx
@@ -0,0 +1,11 @@
+import './Symbol.css'
+
+const Symbol = () => {
+  return (
+    <div className="container--main">
+      <div className="yin-yang"></div>
+    </div>
+  )
+}
+
+export default Symbol
