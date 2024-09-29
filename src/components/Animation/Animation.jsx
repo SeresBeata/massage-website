f7563f2b3830ab80709061f1d59219ff497fdfc5
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-09-29 14:30:36 +0200

    Create Animation component
    Create and export Animation child component. Use style from Animation.css file.

---
+++ src/components/Animation/Animation.jsx
@@ -0,0 +1,69 @@
+import './Animation.css'
+
+const Animation = () => {
+  return (
+    <div className="container--main">
+      <div className="container">
+        <div className="square black">
+          <div className="square">
+            <div className="square black">
+              <div className="square">
+                <div className="square black">
+                  <div className="square">
+                    <div className="square black">
+                      <div className="square">
+                        <div className="square black">
+                          <div className="square">
+                            <div className="square black">
+                              <div className="square">
+                                <div className="square black">
+                                  <div className="square">
+                                    <div className="square black">
+                                      <div className="square">
+                                        <div className="square black">
+                                          <div className="square">
+                                            <div className="square black">
+                                              <div className="square">
+                                                <div className="square black">
+                                                  <div className="square">
+                                                    <div className="square black">
+                                                      <div className="square">
+                                                        <div className="square black">
+                                                          <div className="square">
+                                                            <div className="square black">
+                                                              <div className="square">
+                                                                <div className="square black"></div>
+                                                              </div>
+                                                            </div>
+                                                          </div>
+                                                        </div>
+                                                      </div>
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Animation
